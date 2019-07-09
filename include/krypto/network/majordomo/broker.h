f95c98a56956115890fb61c096d8e764a1fddbde
Kapil Sharma <kapilsharma.frm@gmail.com>
2019-07-08 22:17:04 -0500

    Broker Api
    - Added service and worker encapsulations

--- include/krypto/network/majordomo/broker.h
+++ include/krypto/network/majordomo/broker.h
@@ -1,8 +1,36 @@
-//
-// Created by ksharma on 6/28/19.
-//
+#include <utility>
 
-#ifndef KRYPTO_BROKER_H
-#define KRYPTO_BROKER_H
+#include <utility>
 
-#endif //KRYPTO_BROKER_H
+#pragma once
+
+#include <string>
+#include <memory>
+#include <deque>
+#include <zmq.hpp>
+
+namespace krypto::network::mdp {
+    struct Worker;
+
+    class Service final {
+        std::string name_;
+        std::deque<zmq::message_t> requests_;
+        std::vector<std::unique_ptr<Worker>> workers_;
+
+    public:
+        Service(std::string name);
+    };
+
+    struct Worker final {
+        std::string identity;
+        std::unique_ptr<Service> service;
+        uint64_t expiry;
+
+        Worker(std::string identity, std::unique_ptr<Service> &&service, uint64_t expiry);
+    };
+
+    class Broker final {
+
+    };
+
+}
