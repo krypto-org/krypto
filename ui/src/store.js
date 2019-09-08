0c7dc9623a889ac1416bbaf4b776071c17fd9a14
Kapil Sharma <kapilsh@users.noreply.github.com>
2019-09-08 10:17:49 -0500

    Added websocket middleware
    - Fixed the directory babel config
    - Added websocket middleware

--- ui/src/store.js
+++ ui/src/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import logger from "redux-logger"
+import reduxWebsocket from "@giantmachines/redux-websocket";
 import rootReducer from "./reducers"
 
-const middleware = [thunk, logger];
+const reduxWebsocketMiddleware = reduxWebsocket();
+
+const middleware = [thunk, logger, reduxWebsocketMiddleware];
 
 const store = createStore(
   rootReducer,
