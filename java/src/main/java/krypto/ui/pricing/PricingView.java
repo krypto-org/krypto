62fe026d2f62ab5aa1829a0a184f377b0e4e345d
Kapil Sharma <kapilsharma.frm@gmail.com>
2020-04-25 09:42:01 -0500

    Initial work on pricing window
    - Added new window
    - Added table

--- java/src/main/java/krypto/ui/pricing/PricingView.java
+++ java/src/main/java/krypto/ui/pricing/PricingView.java
@@ -1,7 +1,31 @@
 package krypto.ui.pricing;
 
-import javax.swing.JFrame;
+
+import krypto.ui.UIDataCache;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class PricingView extends JFrame {
-    
+    private final JPanel contentPane;
+
+    private final JScrollPane instrumentsTableSP = new JScrollPane();
+    private final TheoreticalSnapshotTable theoreticalSnapshotTable;
+    private final TheoreticalSnapshotTable instrumentsTable;
+
+    public PricingView(final UIDataCache uiDataCache)  {
+        this.setTitle("PRICING");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 800, 500);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(
+                new MigLayout("",
+                        "[fill][grow]",
+                        "[fill][grow]"));
+
+    }
 }
--- java/src/main/java/krypto/ui/pricing/TheoreticalSnapshotTable.java
+++ java/src/main/java/krypto/ui/pricing/TheoreticalSnapshotTable.java
@@ -1,8 +1,37 @@
 package krypto.ui.pricing;
 
+import krypto.ui.components.ColorConstants;
+import krypto.ui.components.ColoredColumnTableCellRenderer;
+import krypto.ui.mktdata.MktdataSheetTable;
+
 import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TheoreticalSnapshotTable extends JTable {
+
+    private static final Map<Column, Color> COLUMN_COLORS;
+
+    static {
+        COLUMN_COLORS = new HashMap<>();
+        COLUMN_COLORS.put(Column.MM_BID, ColorConstants.BID_PRICE_COLUMN_COLOR);
+        COLUMN_COLORS.put(Column.MM_ASK, ColorConstants.ASK_PRICE_COLUMN_COLOR);
+        COLUMN_COLORS.put(Column.BID_QUANTITY, ColorConstants.BID_QUANTITY_COLUMN_COLOR);
+        COLUMN_COLORS.put(Column.ASK_QUANTITY, ColorConstants.ASK_QUANTITY_COLUMN_COLOR);
+    }
+
+    public TheoreticalSnapshotTable(TableModel dm, TableCellRenderer theoCellRenderer) {
+        super(dm);
+        COLUMN_COLORS.forEach((column, color) -> {
+            this.getColumnModel().getColumn(column.ordinal()).setCellRenderer(
+                    new ColoredColumnTableCellRenderer(color, Color.WHITE, column.ordinal(), true));
+        });
+        this.getColumnModel().getColumn(Column.THEO.ordinal()).setCellRenderer(theoCellRenderer);
+        this.getColumnModel().getColumn(Column.ADJUSTED_THEO.ordinal()).setCellRenderer(theoCellRenderer);
+    }
 
     public enum Column {
         INSTRUMENT,
